f3924a95107e788511051ba1c9de5ea55a719bcf
mrisqid <mrisqid@gmail.com>
2024-03-26 18:19:40 +0700

    bug: fix logout just redirect to login but not clear the cache

--- apps/admin/src/components/wrapper/actions.ts
+++ apps/admin/src/components/wrapper/actions.ts
@@ -1,7 +1,7 @@
 'use server'
 
 import { redirect } from 'next/navigation'
-import { createClient } from '../../utils/supabase/client'
+import { createClient } from '../../utils/supabase/server'
 
 export async function logout() {
   const supabase = createClient()
