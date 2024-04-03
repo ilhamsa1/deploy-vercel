86ff66fe23d9c81e5d100e6b97e1ea2057432630
Muhammad ilham <muhammad.ilhamsa1@gmail.com>
2024-04-03 22:44:25 +0700

    feat: setup react query

--- apps/admin/src/components/shared-provider/index.tsx
+++ apps/admin/src/components/shared-provider/index.tsx
@@ -5,12 +5,15 @@ import { AppProgressBar as ProgressBar } from 'next-nprogress-bar'
 import '@fontsource-variable/open-sans'
 import 'react-day-picker/dist/style.css'
 
+import ReactQueryProvider from './react-query-provider'
 import MuiThemeProvider from './mui-theme-provider'
 
 const SharedProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <MuiThemeProvider>{children}</MuiThemeProvider>
+      <ReactQueryProvider>
+        <MuiThemeProvider>{children}</MuiThemeProvider>
+      </ReactQueryProvider>
 
       <ProgressBar
         height="4px"
