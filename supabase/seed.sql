9186f3cc1eac4bc5549edc1f0c3d1fd5450a7116
mrisqid <mrisqid@gmail.com>
2024-04-05 00:36:58 +0700

    refactor: changes some tables entity, add more RLS for some tables policy

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -4,3 +4,9 @@ public.usage_tier (display_name)
 VALUES
 ('super'),
 ('regular');
+-- insert default org
+INSERT INTO
+public.org (tier, tag, display_name)
+VALUES
+(1, 'rampable', 'Rampable'),
+(2, 'lexupay', 'Lexupay');
