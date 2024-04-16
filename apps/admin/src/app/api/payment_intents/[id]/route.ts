eef4224e3654506b33f74c79a3d601ff1412bbcc
David Maulana <davidmaulana10k@gmail.com>
2024-04-16 23:42:24 +0700

    get data by params

--- apps/admin/src/app/api/payment_intents/[id]/route.ts
+++ apps/admin/src/app/api/payment_intents/[id]/route.ts
@@ -5,7 +5,16 @@ export async function GET(request: Request, context: { params: { id: string } })
   const paymentIntentId = context.params.id
   const supabase = createClient()
 
-  const { data } = await supabase.from('payment_intent').select().eq('id', paymentIntentId).single()
+  const { data: userData } = await supabase.auth.getUser()
+
+  if (!userData?.user) throw new Error('No Authorization')
+
+  const { data } = await supabase
+    .from('payment_intent')
+    .select()
+    .eq('id', paymentIntentId)
+    .eq('userId', userData?.user.id)
+    .single()
 
   return NextResponse.json({ data })
 }
