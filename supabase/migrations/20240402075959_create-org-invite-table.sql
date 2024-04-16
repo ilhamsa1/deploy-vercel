3c3b4b5cf1062c4da80f019e5d33caffccd39763
mrisqid <mrisqid@gmail.com>
2024-04-16 18:48:12 +0700

    refactor: auto generate invite code

--- supabase/migrations/20240402075959_create-org-invite-table.sql
+++ supabase/migrations/20240402075959_create-org-invite-table.sql
@@ -1,9 +1,31 @@
+-- Enable the "pgsodium" extension
+CREATE EXTENSION IF NOT EXISTS pgsodium;
+
+-- Enable the "pgcrypto" extension
+CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA extensions;
+
+-- Enable the "pg_hashids" extension
+CREATE EXTENSION IF NOT EXISTS pg_hashids WITH SCHEMA extensions;
+
+-- Create the gen_short_secret(key_realm, key_rotate_idx, key_length) function
+CREATE OR REPLACE FUNCTION gen_short_secret(char(8), int8 DEFAULT 1, integer DEFAULT 56)
+RETURNS bytea AS $$
+  -- Use a server-managed secret key (with optional key-rotation)
+  SELECT pgsodium.derive_key($2, $3, $1::bytea);
+$$ LANGUAGE sql IMMUTABLE STRICT PARALLEL SAFE;
+
+-- Create the gen_short_code(id, secret, min_char_length, custom_alphabet_set) function
+CREATE OR REPLACE FUNCTION gen_short_code(int8, bytea, integer DEFAULT 6, varchar(512) DEFAULT 'abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ123456789')
+RETURNS text AS $$
+  SELECT id_encode(('x' || encode(encrypt(int8send($1), $2, 'bf-ecb/pad:none'), 'hex'))::bit(64)::int8, '', $3, $4);
+$$ LANGUAGE sql IMMUTABLE STRICT PARALLEL SAFE;
+
 -- create a public.org_invite table for storing invitation to join org
 CREATE TABLE public.org_invite (
-  id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
+  id BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY,
   org_id INTEGER NOT NULL REFERENCES org ON DELETE CASCADE,
   created_by UUID NOT NULL,
-  code TEXT,
+  code TEXT UNIQUE GENERATED ALWAYS AS (gen_short_code(id, gen_short_secret('_invite_'))) STORED,
   send_to TEXT,
   role TEXT,
   accepted_at TIMESTAMPTZ,
