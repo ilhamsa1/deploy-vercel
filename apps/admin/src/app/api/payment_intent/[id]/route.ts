87d21506cb6fb8579e78662979cb6e28d467ce5a
David Maulana <davidmaulana10k@gmail.com>
2024-04-17 20:06:04 +0700

    fix logic api payment intent

--- apps/admin/src/app/api/payment_intent/[id]/route.ts
+++ apps/admin/src/app/api/payment_intent/[id]/route.ts
@@ -1,11 +1,18 @@
 import { NextResponse } from 'next/server'
 import { createClient } from '@/utils/supabase/server'
+import { serviceHandler } from '@/services/service-handler'
 
 export async function GET(request: Request, context: { params: { id: string } }) {
   const paymentIntentId = context.params.id
   const supabase = createClient()
+  const accountId = await serviceHandler(request)
 
-  const { data } = await supabase.from('payment_intent').select().eq('id', paymentIntentId).single()
+  const { data } = await supabase
+    .from('payment_intent')
+    .select()
+    .eq('id', paymentIntentId)
+    .eq('account_id', accountId)
+    .single()
 
   return NextResponse.json({ data })
 }
--- apps/admin/src/app/api/payment_intent/route.ts
+++ apps/admin/src/app/api/payment_intent/route.ts
@@ -1,22 +1,27 @@
 import { NextResponse } from 'next/server'
 import { createClient } from '@/utils/supabase/server'
+import { serviceHandler } from '@/services/service-handler'
 
-export async function GET() {
+export async function GET(request: Request) {
   const supabase = createClient()
+  const accountId = await serviceHandler(request)
 
   const { data, count } = await supabase
     .from('payment_intent')
     .select('*', { count: 'exact', head: true })
+    .eq('account_id', accountId)
+
   return NextResponse.json({ data, count })
 }
 
 export async function POST(request: Request) {
   const supabase = createClient()
   const params = await request.json()
+  const accountId = await serviceHandler(request)
 
   const { data } = await supabase
     .from('payment_intent')
-    .insert(params)
+    .insert({ ...params, accountId })
     .select('*')
     .throwOnError()
     .single()
