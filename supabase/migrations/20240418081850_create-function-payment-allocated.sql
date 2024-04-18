9c102110011a682e2b2f233fd76ebbfe026d7eef
Muhammad ilham <muhammad.ilhamsa1@gmail.com>
2024-04-18 16:59:04 +0700

    feat: assign bank account in payment intent

--- supabase/migrations/20240418081850_create-function-payment-allocated.sql
+++ supabase/migrations/20240418081850_create-function-payment-allocated.sql
@@ -1,10 +1,13 @@
 CREATE OR REPLACE FUNCTION public.allocate_payment_method_single(item public.payment_intent) RETURNS BOOLEAN AS $$
 DECLARE
     row payment_intent;
+    row_bank_account bank_account;
 BEGIN
 
     SELECT * INTO STRICT row 
-    FROM payment_intent 
+    FROM payment_intent
+      JOIN business_account
+      on business_account.id = payment_intent.account_id
     WHERE id = item.id AND status = 'require_payment_method' 
     LIMIT 1 
     FOR UPDATE SKIP LOCKED;
@@ -13,8 +16,24 @@ BEGIN
         RETURN FALSE;
     END IF;
 
+    -- Get available bank account
+    
+    SELECT * FROM bank_account INTO STRICT row_bank_account
+    JOIN bank
+    ON bank.id = bank_account.bank_id
+    WHERE bank_account.org_id = row.id
+    ORDER BY bank_account.last_allocated_at DESC
+    LIMIT 1
+    FOR UPDATE SKIP LOCKED;
+
     UPDATE payment_intent 
-    SET status = 'requires_action'
+    SET 
+      status = 'requires_action',
+      payment_method = CONCAT(tag, '_', row_bank_account.id) 
+    WHERE id = row.id;
+
+    UPDATE bank_account 
+    SET last_allocated_at = NOW()
     WHERE id = row.id;
 
     RETURN TRUE;
