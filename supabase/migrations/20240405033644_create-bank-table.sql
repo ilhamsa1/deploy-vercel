59492b2aad0dfa133b6a7f0fb5b4e1049c35d622
mrisqid <mrisqid@gmail.com>
2024-04-18 15:06:57 +0700

    remove: org id in bank table

--- supabase/migrations/20240405033644_create-bank-table.sql
+++ supabase/migrations/20240405033644_create-bank-table.sql
@@ -1,7 +1,6 @@
 -- create a public.bank table for storing bank information
 CREATE TABLE public.bank (
   id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
-  org_id INTEGER NOT NULL REFERENCES org ON DELETE CASCADE,
   tag TEXT,
   name TEXT,
   PRIMARY KEY (id)
