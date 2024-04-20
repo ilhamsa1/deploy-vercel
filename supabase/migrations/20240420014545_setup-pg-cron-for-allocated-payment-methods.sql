ecc791a870577880007c19c97762939046d10979
Muhammad ilham <muhammad.ilhamsa1@gmail.com>
2024-04-20 16:46:14 +0700

    feat: add grant to postgres as default

--- supabase/migrations/20240420014545_setup-pg-cron-for-allocated-payment-methods.sql
+++ supabase/migrations/20240420014545_setup-pg-cron-for-allocated-payment-methods.sql
@@ -1,5 +1,8 @@
 create extension if not exists pg_cron;
 
+grant usage on schema cron to postgres;
+grant all privileges on all tables in schema cron to postgres;
+
 select
   cron.schedule(
     'scan-allocated-payment-method', 
