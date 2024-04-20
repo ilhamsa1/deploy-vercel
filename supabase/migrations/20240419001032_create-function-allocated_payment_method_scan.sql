47618e503004883cca115929d1cd36f71559d2ed
Muhammad ilham <muhammad.ilhamsa1@gmail.com>
2024-04-20 08:08:12 +0700

    fix: move the query order of allocate_payment_method

--- supabase/migrations/20240419001032_create-function-allocated_payment_method_scan.sql
+++ supabase/migrations/20240419001032_create-function-allocated_payment_method_scan.sql
@@ -1,13 +1,8 @@
-CREATE OR REPLACE FUNCTION public.allocate_payment_method(item public.payment_intent DEFAULT NULL) RETURNS VOID AS $$
+CREATE OR REPLACE FUNCTION public.allocate_payment_methods(item public.payment_intent DEFAULT NULL) RETURNS VOID AS $$
 DECLARE
     each_item payment_intent;  -- Declare as a record type for each row
 BEGIN
     
-    -- If item is provided, process it as well
-    IF item IS NOT NULL THEN
-        PERFORM allocate_payment_method_single(item);
-    END IF;
-
     -- Scan items that are not yet being processed
     FOR each_item IN
         SELECT * FROM payment_intent WHERE status = 'require_payment_method'
@@ -15,5 +10,10 @@ BEGIN
         -- Process each pending item
         PERFORM allocate_payment_method_single(each_item);
     END LOOP;
+
+    -- If item is provided, process it as well
+    IF item IS NOT NULL THEN
+        PERFORM allocate_payment_method_single(item);
+    END IF;
 END;
 $$ LANGUAGE plpgsql;
