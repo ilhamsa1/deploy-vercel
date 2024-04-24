51b73bbc33f2154746e29bbef0d149ed175aec97
David Maulana <davidmaulana10k@gmail.com>
2024-04-24 16:47:20 +0700

    fix post payment intent

--- apps/admin/src/app/api/payment_intent/[id]/route.ts
+++ apps/admin/src/app/api/payment_intent/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import { apiKey } from '@/services/api-key'
+import { createClientWithAuthHeader } from '@/utils/supabase/server'
 
 // Change the dynamic behavior of a layout or page to fully static or fully dynamic. https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#dynamic
 export const dynamic = 'auto'
@@ -10,7 +10,7 @@ export const fetchCache = 'auto'
 
 export async function GET(request: Request, context: { params: { id: string } }) {
   const paymentIntentId = context.params.id
-  const supabase = await apiKey(request)
+  const supabase = await createClientWithAuthHeader()
 
   const { data } = await supabase
     .from('payment_intent')
