9cb4b5241d76242316ed73eb73cd7c6fe5fafb94
Muhammad ilham <muhammad.ilhamsa1@gmail.com>
2024-04-27 15:00:42 +0700

    feat: run webhook provider when event is created

---
+++ supabase/migrations/20240426040514_create-event-handle-payment-intent-change.sql
@@ -0,0 +1,33 @@
+CREATE OR REPLACE FUNCTION private.handle_event_inserted()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+SECURITY definer 
+SET search_path = private
+AS $$
+DECLARE
+  webhook_endpoint_item RECORD;
+BEGIN
+  -- Retrieve webhook endpoints for the organization and enabled event
+  FOR webhook_endpoint_item IN 
+    SELECT * 
+    FROM webhook_endpoint 
+    WHERE org_id = NEW.org_id 
+      AND NEW.event_type = ANY(enabled_events)
+  LOOP
+    -- Call request_wrapper for each webhook endpoint
+    PERFORM request_wrapper(
+      'POST', 
+      webhook_endpoint_item.url,
+      undefined,
+      NEW
+    );
+  END LOOP;
+
+  RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER event_insert_trigger
+AFTER INSERT ON event
+FOR EACH ROW
+EXECUTE FUNCTION private.handle_event_inserted();
