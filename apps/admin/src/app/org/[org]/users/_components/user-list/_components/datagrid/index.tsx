7d4c0aa52adfa100968cc8fd6ac36dde825d75f7
David Maulana <davidmaulana10k@gmail.com>
2024-04-30 00:15:50 +0700

    make component for datagrid cursor based

--- apps/admin/src/app/org/[org]/users/_components/user-list/_components/datagrid/index.tsx
+++ apps/admin/src/app/org/[org]/users/_components/user-list/_components/datagrid/index.tsx
@@ -1,22 +1,9 @@
 import Typography from '@mui/material/Typography'
-import {
-  GridCellParams,
-  GridPaginationModel,
-  GridRowModel,
-  GridPaginationMeta,
-  DataGrid,
-  GridSortModel,
-} from '@mui/x-data-grid'
-import React, {
-  Dispatch,
-  SetStateAction,
-  useCallback,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-} from 'react'
+import { GridCellParams, GridPaginationModel, GridRowModel, GridSortModel } from '@mui/x-data-grid'
+import React, { Dispatch, SetStateAction } from 'react'
 import Box from '@mui/material/Box'
+
+import DatagridCursorBased from '@/components/datagrid-cursor-based'
 
 import { formatDateNameShortMonth } from '@/lib/date'
 
@@ -43,12 +30,6 @@ const List = ({
   setPaginationModel,
   getPageCursors,
 }: Props) => {
-  // Some API clients return undefined while loading
-  // Following lines are here to prevent `rowCountState` from being undefined during the loading
-  const [rowCountState, setRowCountState] = useState(totalRowCount || 0)
-  const prevEstimatedRowCount = useRef<number | undefined>(undefined)
-  const paginationMetaRef = useRef<GridPaginationMeta>()
-
   const columns = [
     {
       field: 'display_name',
@@ -112,85 +93,22 @@ const List = ({
     },
   ]
 
-  // This handler is called when the next page button is pressed
-  const handlePaginationModelChange = useCallback(
-    (newPaginationModel: GridPaginationModel) => {
-      const currentPage = paginationModel.page
-      const targetPage = newPaginationModel.page
-
-      const [, nextCursor] = getPageCursors(targetPage - 1, true) || []
-      const [prevCursor] = getPageCursors(targetPage + 1, true) || []
-
-      if ((targetPage > currentPage && nextCursor) || prevCursor) {
-        // If we have the next_cursor, we can allow the page to change.
-        setOriginPaginationModel(paginationModel)
-        setPaginationModel(newPaginationModel)
-      } else if ((targetPage < currentPage && prevCursor) || nextCursor) {
-        // If we have the prev_cursor, we can allow the page to change.
-        setOriginPaginationModel(paginationModel)
-        setPaginationModel(newPaginationModel)
-      } else if (targetPage === currentPage) {
-        // If page not changed, we allow only the pageSize to change.
-        setOriginPaginationModel(paginationModel)
-        setPaginationModel(newPaginationModel)
-      } else {
-        // Else, ignore the change.
-        setPaginationModel({ ...paginationModel }) // trigger reload the same page
-      }
-    },
-    [paginationModel, setPaginationModel, setOriginPaginationModel, getPageCursors],
-  )
-
-  const handleSortModelChange = useCallback((sortModel: GridSortModel) => {
-    // Here you save the data you need from the sort model
-    setSortModel([...sortModel])
-  }, [])
-
-  // Memoize to avoid flickering when the `hasNextPage` is `undefined` during refetch
-  const paginationMeta = useMemo(() => {
-    if (hasNextPage !== undefined && paginationMetaRef.current?.hasNextPage !== hasNextPage) {
-      paginationMetaRef.current = { hasNextPage }
-    }
-    return paginationMetaRef.current
-  }, [hasNextPage])
-
-  useEffect(() => {
-    if (paginationMeta?.hasNextPage) setRowCountState(-1)
-  }, [paginationMeta?.hasNextPage])
-
-  useEffect(() => {
-    if (totalRowCount) setRowCountState(totalRowCount)
-  }, [totalRowCount])
-
-  // Memoize to avoid flickering when the `totalRowCount` is `0` during refetch
-  const estimatedRowCount = useMemo(() => {
-    if (totalRowCount !== 0) {
-      if (prevEstimatedRowCount.current === undefined) {
-        prevEstimatedRowCount.current = totalRowCount
-      }
-      return totalRowCount
-    }
-    return prevEstimatedRowCount.current
-  }, [totalRowCount])
-
   return (
     <Box sx={{ mt: '1rem' }}>
-      <DataGrid
+      <DatagridCursorBased
         rows={users}
         autoHeight
+        noAction
         columns={columns}
-        sortingMode="server"
-        paginationMode="server"
-        pageSizeOptions={[10, 20, 50, 100]}
-        onSortModelChange={handleSortModelChange}
         loading={isLoading}
         getRowId={(row: GridRowModel) => row.user_id}
         paginationModel={paginationModel}
-        onPaginationModelChange={handlePaginationModelChange}
-        paginationMeta={paginationMeta}
-        rowCount={rowCountState}
-        onRowCountChange={(newRowCount) => setRowCountState(newRowCount)}
-        estimatedRowCount={estimatedRowCount}
+        setPaginationModel={setPaginationModel}
+        setOriginPaginationModel={setOriginPaginationModel}
+        getPageCursors={getPageCursors}
+        hasNextPage={hasNextPage}
+        totalRowCount={totalRowCount}
+        setSortModel={setSortModel}
       />
     </Box>
   )
