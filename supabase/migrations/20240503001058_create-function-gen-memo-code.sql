07e27d3de4af746b6f6cb0ef3af2789cfc7a103b
Muhammad ilham <muhammad.ilhamsa1@gmail.com>
2024-05-03 09:09:47 +0700

    feat: generated memo

---
+++ supabase/migrations/20240503001058_create-function-gen-memo-code.sql
@@ -0,0 +1,21 @@
+-- Enable the "pgsodium" extension
+CREATE EXTENSION IF NOT EXISTS pgsodium;
+
+-- Use a server-managed secret key (with key-rotation)
+CREATE OR REPLACE FUNCTION gen_mask_secret(char(8), int8 DEFAULT 1, integer DEFAULT 16)
+RETURNS bytea AS $$
+  -- gen_mask_secret(key_realm, rotate_idx, key_length)
+  -- key_length: `32` for 256-bit key
+  SELECT pgsodium.derive_key($2, $3, $1::bytea);
+$$ LANGUAGE sql IMMUTABLE STRICT PARALLEL SAFE;
+
+CREATE OR REPLACE FUNCTION public.gen_memo_code(numeric, integer DEFAULT 6, varchar(512) DEFAULT '')
+RETURNS text AS $$
+  -- gen_mask_code(id, min_char_length, tweak_or_seed)
+  WITH t1(num_text) AS (
+    VALUES (trunc($1)::text)
+  ), t2(num_text) AS (
+    SELECT (CASE WHEN length(num_text) < $2 THEN lpad(num_text, $2, '0') ELSE num_text END) FROM t1
+  )
+  SELECT array_to_string(ff1_encrypt(regexp_split_to_array(num_text, '')::integer[], 10, gen_mask_secret('_memo___'), $3::bytea), '') FROM t2 LIMIT 1;
+$$ LANGUAGE sql IMMUTABLE STRICT PARALLEL SAFE;
---
+++ supabase/migrations/20240503001641_create-bank-payment-memo.sql
@@ -0,0 +1,17 @@
+CREATE SEQUENCE public.bank_payment_memo_seq;
+
+-- create a public.payment_tx table for storing payment transaction information
+CREATE TABLE public.bank_payment_memo (
+  -- id UUID_ULID NOT NULL DEFAULT uuid_generate_v7(),
+  ba_id INTEGER NOT NULL REFERENCES bank_account ON DELETE CASCADE,
+  pi_id UUID NOT NULL REFERENCES payment_intent ON DELETE CASCADE,
+  seq BIGINT DEFAULT nextval('bank_payment_memo_seq'),
+  code TEXT GENERATED ALWAYS AS (public.gen_memo_code(seq)) STORED,
+
+  CONSTRAINT pk_bank_payment_memo PRIMARY KEY (pi_id, ba_id),
+  
+  CONSTRAINT uq_bank_payment_memo_ba_seq UNIQUE (ba_id, seq),
+  CONSTRAINT uq_bank_payment_memo_ba_code UNIQUE (ba_id, code)
+);
+-- enable RLS, we want to restrict access on this table
+ALTER TABLE public.bank_payment_memo ENABLE ROW LEVEL SECURITY;
